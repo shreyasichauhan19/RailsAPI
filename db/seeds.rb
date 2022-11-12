aca33f8cfb8c66fe58a521174b83764f0c8970ce
Shreyasi Chauhan <chauhan.shreyasi19@gmail.com>
2022-11-11 20:13:03 -0800

    added custom data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create([
+    {
+        name: "qwertyuiop",
+        brand: "qwertyuiop",
+        price:"qwertyuiop",
+        description:"qwertyuiop"
+    },
+    {
+        name: "wwwwwww",
+        brand: "ssssssssss",
+        price:"qwertyuiop",
+        description:"ddddddddd"
+    },
+    {
+        name: "11111111111",
+        brand: "eeeeeeeee",
+        price:"ddddddddd",
+        description:"ddd444444444dddddd"
+    },
+    {
+        name: "1111111",
+        brand: "ssssssssss",
+        price:"dddddwsswssddjdncd",
+        description:"ddddddddd"
+
+    }
+])
